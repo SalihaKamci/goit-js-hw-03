351154ac4c43ece658303b7bb817ddaa5934537f
saliha <salihakamci06@gmail.com>
2025-05-15 23:55:58 +0300

    .

--- js/task-1.js
+++ js/task-1.js
@@ -1,1 +1,4 @@
-
+function slugify(title){
+    return title.toLowerCase().split(" ").join("-");
+}
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS"));
--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,23 @@
-
+function makeArray(firstArray, secondArray, maxLength) {
+    let newArray = [];
+    if (firstArray.length + secondArray.length <= maxLength) {
+        return newArray = firstArray.concat(secondArray);
+    }
+    else {
+        if (maxLength === 0) {
+            return newArray;
+        }
+        newArray = newArray.concat(firstArray);
+        let a = maxLength - newArray.length;
+        for (let i = 0; i < a; i++) {
+            newArray.push(secondArray[i]);
+        }
+        return newArray;
+    }
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
